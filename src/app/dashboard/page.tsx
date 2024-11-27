55c20a3fcc210ba05abbc6d6b48d99b452623e8a
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-11-27 08:20:31 -0500

    adding a position table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,45 @@
 import { PortfolioValuation } from "@/components/portfolio-metrics/portfolio-valuation";
+import { PositionsTable } from "@/components/portfolio-metrics/position-table";
 
 export default function DashboardPage() {
   // TODO: This is a dummy data
   const portfolioValue = 10000;
   const totalReturn = 0.05;
 
+  const positions = [
+    {
+      id: "1",
+      symbol: "ES",
+      quantity: 100,
+      entryPrice: 3000.0,
+      currentPrice: 3010.0,
+      pnl: 1000.0,
+      returnPercent: 0.2,
+    },
+    {
+      id: "1",
+      symbol: "ES",
+      quantity: 100,
+      entryPrice: 3000.0,
+      currentPrice: 3010.0,
+      pnl: 1000.0,
+      returnPercent: 0.2,
+    },
+    {
+      id: "1",
+      symbol: "ES",
+      quantity: 100,
+      entryPrice: 3000.0,
+      currentPrice: 3010.0,
+      pnl: 1000.0,
+      returnPercent: 0.2,
+    },
+  ];
+
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-start justify-center space-y-6">
       <PortfolioValuation value={portfolioValue} totalReturn={totalReturn} />
+      <PositionsTable positions={positions} />
     </div>
   );
 }
