f33eae2813448a68608e2970501f719a2098f132
BonelessWater <domdd305@gmail.com>
2025-04-18 15:22:56 -0400

    Dockerfile

--- amplify/functions/resource.ts
+++
@@ -1,9 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const appFunction = defineFunction({
-  // this name becomes the CloudFormation logical ID
-  name: 'backendApp',
-  // path to your handler; relative to this file
-  entry: '../../../backend/app.py',
-  runtime: 'python3.9',
-});
